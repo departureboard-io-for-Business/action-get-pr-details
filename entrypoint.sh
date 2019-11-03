8766f6cf400949ee3f585941f0ffdf89cd4c143f
benjamin-maynard <383838-github@ea.maynard.io>
2019-11-03 23:29:24 +0000

    update branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ PRURL=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.html_url')
 PRID=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.number')
 PRCREATOR=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.user.id')
 
-PRHEAD=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.head.label')
+PRBRANCH=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.head.label')
 PRACTION=$(cat ${GITHUB_EVENT_PATH} | jq -r '.action')
 PRMERGED=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.merged')
 PRHEADSHA=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.head.sha')
@@ -13,7 +13,7 @@ echo ::set-output name=prurl::$PRURL
 echo ::set-output name=prid::$PRID
 echo ::set-output name=prcreator::$PRCREATOR
 
-echo ::set-output name=prhead::$PRHEAD
+echo ::set-output name=prbranch::$PRBRANCH
 echo ::set-output name=praction::$PRACTION
 echo ::set-output name=prmerged::$PRMERGED
 echo ::set-output name=prheadsha::$PRHEADSHA
