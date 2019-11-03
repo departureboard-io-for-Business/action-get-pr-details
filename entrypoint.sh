c0bcdc9c19dbeb3876d2de74a166e836fe1ce57b
benjamin-maynard <383838-github@ea.maynard.io>
2019-11-03 23:25:30 +0000

    Add additional info

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,16 @@ PRURL=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.html_url')
 PRID=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.number')
 PRCREATOR=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.user.id')
 
+PRHEAD=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.head.label')
+PRACTION=$(cat ${GITHUB_EVENT_PATH} | jq -r '.action')
+PRMERGED=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.merged')
+PRHEADSHA=$(cat ${GITHUB_EVENT_PATH} | jq -r '.pull_request.head.sha')
+
 echo ::set-output name=prurl::$PRURL
 echo ::set-output name=prid::$PRID
 echo ::set-output name=prcreator::$PRCREATOR
+
+echo ::set-output name=prhead::$PRHEAD
+echo ::set-output name=praction::$PRACTION
+echo ::set-output name=prmerged::$PRMERGED
+echo ::set-output name=prheadsha::$PRHEADSHA
